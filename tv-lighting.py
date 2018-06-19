cb957b2e9eac7bb158e475b94db6fedf28519d03
Siequnu <11821606+Siequnu@users.noreply.github.com>
2018-06-19 10:05:59 +0800

    Add Philips Hue support

--- tv-lighting.py
+++ tv-lighting.py
@@ -4,8 +4,13 @@ import sys
 import time
 
 # Turn on lights
+# Living room strip
 turn_on_command = """curl -X PUT --header "Content-Type:Application/json" --header "authorization: 092-94-999" http://192.168.31.95:51826/characteristics --data '{"characteristics":[{"aid":41,"iid":10,"value":true,"status":0}]}'"""
 process = subprocess.Popen(turn_on_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+# Philips long table
+turn_on_command = """curl -X PUT --header "Content-Type:Application/json" http://192.168.31.176/api/X-evzKmA8ysqS6rSO9mheet8r4eOGSRYAJ5UvUuJ/lights/2/state --data '{"on":true}'"""
+process = subprocess.Popen(turn_on_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+
 
 while True:
 	try:	
@@ -49,6 +54,10 @@ while True:
 		cmd_dining_table = """curl -X PUT --header "Content-Type:Application/json" --header "authorization: 092-94-999" http://192.168.31.95:51826/characteristics --data '{"characteristics":[{"aid":38,"iid":12,"value":""" + str(hue) + """},{"aid":38,"iid":13,"value":""" + str(saturation) + """ }]}'"""
 		process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
 		#process = subprocess.Popen(cmd_dining_table, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+		
+		philips_hue = int(float(hue) * 65535 / 365)
+		cmd_philips_hue = """curl -X PUT --header "Content-Type:Application/json" http://192.168.31.176/api/X-evzKmA8ysqS6rSO9mheet8r4eOGSRYAJ5UvUuJ/lights/2/state --data '{"on":true, "sat":254, "bri":254,"hue":""" + str(philips_hue) + """}'"""
+		process = subprocess.Popen(cmd_philips_hue, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
 	except:
 		pass
 	
